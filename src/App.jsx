585890973c1e902903fadb7830cdc54744b5b25c
LaikamSamanta <laikamsamanta@gmail.com>
2025-07-30 00:31:44 +0300

    everything is working

--- src/App.jsx
+++ src/App.jsx
@@ -15,39 +15,39 @@ function App() {
         <Navbar />
       </div>
       <div className="sections">
-      <section id="hero-section" className="relative">
-       <AnimatedGridPattern className="animated-grid absolute inset-0 z-0" />
-        <Hero />
-      </section>
-      <section id="about">
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold mb-4">About Me</h2>
-          <p className="text-lg">
-            This is the about section where you can write about yourself.
-          </p>
-        </div>
-      </section>
-      <section id="skills">
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold mb-4">Skills</h2>
-          <ClientLogo />
-        </div>
-      </section>
-      <section id="projects">
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold mb-4">Projects</h2>
-          <p className="text-lg">
-            This is the projects section where you can showcase your projects.
-          </p>
-          <MyProjects />
-        </div>
-      </section>
-      <section id="contact">
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
-          <Contact />
-        </div>
-      </section>
+        <section id="hero-section" className="relative">
+            <AnimatedGridPattern className="animated-grid absolute inset-0 z-0" />
+            <Hero />
+        </section>
+        <section id="about">
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold mb-4">About Me</h2>
+            <p className="text-lg">
+              This is the about section where you can write about yourself.
+            </p>
+          </div>
+        </section>
+        <section id="skills">
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold mb-4">Skills</h2>
+            <ClientLogo />
+          </div>
+        </section>
+        <section id="projects">
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold mb-4">Projects</h2>
+            <p className="text-lg">
+              This is the projects section where you can showcase your projects.
+            </p>
+            <MyProjects />
+          </div>
+        </section>
+        <section id="contact">
+          <div className="container mx-auto px-4 py-8">
+            <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
+            <Contact />
+          </div>
+        </section>
       </div>
     </>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,6 +11,7 @@ export default function Navbar() {
       <ul
         tabIndex={0}
         className="menu-drop menu menu-sm dropdown-content rounded-box z-1 mt-3 w-53 p-2 shadow-lg">
+        <li><a href="hero-section">HOME</a></li>
         <li><a href="#about">ABOUT</a></li>
         <li><a href="#skills">SKILLS</a></li>
         <li><a href="#projects">PROJECTS</a></li>
@@ -25,6 +26,7 @@ export default function Navbar() {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
+      <li><a href="hero-section">HOME</a></li>
       <li><a href="#about">ABOUT</a></li>
       <li><a href="#skills">SKILLS</a></li>
       <li><a href="#projects">PROJECTS</a></li>
@@ -32,7 +34,7 @@ export default function Navbar() {
     </ul>
   </div>
   <div className="navbar-end">
-<a className="download btn btn-outline btn-neutral">Download CV<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5">
+<a className="download btn btn-outline btn-neutral">Download CV<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="download-svg size-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></svg></a>
   </div>
 </div>
