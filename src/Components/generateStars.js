8a0d4ab77a79190f25d7868b0fe7f54165d6c9d0
LaikamSamanta <laikamsamanta@gmail.com>
2025-07-30 23:48:40 +0300

    Working parallax background stars

--- src/Components/generateStars.js
+++ src/Components/generateStars.js
@@ -1,12 +1,13 @@
 export function generateBoxShadows(count) {
   const shadows = [];
   const width = window.innerWidth;
-  const height = window.innerHeight;
+  const height = window.innerHeight * 2;
 
   for (let i = 0; i < count; i++) {
     const x = Math.floor(Math.random() * width);
     const y = Math.floor(Math.random() * height);
-    shadows.push(`${x}px ${y}px #FFF`);
+    const opacity = (Math.random() * 0.5 + 0.5).toFixed(2);
+    shadows.push(`${x}px ${y}px rgba(255, 255, 255, ${opacity})`);
   }
 
   return shadows.join(', ');
