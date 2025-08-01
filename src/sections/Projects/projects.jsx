7f7ceaa2f8fbbfea4e79ea0f75be8331c8547c6d
Samanta Biezēka <84188798+LaikamSamanta@users.noreply.github.com>
2025-08-01 18:22:22 +0300

    Update projects.jsx

--- src/sections/Projects/projects.jsx
+++ src/sections/Projects/projects.jsx
@@ -31,7 +31,7 @@ function ProjectCard({ title, description, imgSrc }) {
 
 export default function MyProjects() {
   return (
-    <section className="h-[90vh] w-full flex items-center justify-center">
+    <section className="h-[80vh] w-full flex items-center justify-center">
       <div className="w-full max-w-7xl px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 h-full">
           <ProjectCard
