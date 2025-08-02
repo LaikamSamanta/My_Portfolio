a0de5c1c79d9a7798aa65359cd1366960533c6d1
LaikamSamanta <laikamsamanta@gmail.com>
2025-08-03 01:15:31 +0300

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
     </ul>
   </div>
   <div className="navbar-end">
-<a className="download btn btn-outline btn-neutral">Download CV<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="download-svg size-4">
+<a className="download btn btn-outline btn-neutral">CV<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="download-svg size-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></svg></a>
   </div>
 </div>
