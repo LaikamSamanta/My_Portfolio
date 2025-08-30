e780d906aaab4d9ba7595117226edd92d126d3db
Samanta Biezēka <sbiezeka@gmail.com>
2025-08-31 00:50:51 +0300

    v.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <section id="hero-section" className="relative bg-base-200 w-full h-screen overflow-hidden">
           
           {/* Sparkles and Eclipse background covering entire hero section */}
-          <div className="absolute inset-0 w-full h-full overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#8350e8,transparent_70%)] before:opacity-40 after:absolute after:-left-1/2 after:top-3/4 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#7876c566] after:bg-zinc-900">
+          <div className="sparkles absolute inset-0 w-full h-full overflow-hidden [mask-image:radial-gradient(50%_50%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_bottom_center,#8350e8,transparent_70%)] before:opacity-40 after:absolute after:-left-1/2 after:top-3/4 after:aspect-[1/0.7] after:w-[200%] after:rounded-[100%] after:border-t after:border-[#7876c566] after:bg-zinc-900">
             <Sparkles
               density={1200}
               className="absolute inset-0 h-full w-full [mask-image:radial-gradient(50%_50%,white,transparent_85%)]"
